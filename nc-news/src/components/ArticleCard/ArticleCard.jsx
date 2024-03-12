c4ba07f89a37a1dfbb3b7661342d8d66add64fc7
Ruby Faye Maria <rubyfayemaria@Rubys-MBP.broadband>
2024-03-12 10:58:45 +0000

    added date to article card

--- nc-news/src/components/ArticleCard/ArticleCard.jsx
+++ nc-news/src/components/ArticleCard/ArticleCard.jsx
@@ -1,4 +1,13 @@
 const ArticleCard = ({article}) => {
+    const date = new Date(article.created_at)
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    const hour = date.getHours();
+    const minutes = date.getMinutes();
+
+    const formattedDate = `${day}/${month}/${year} ${hour}:${minutes}`;
+    
     return (
         <>
             <h4>{article.title}</h4>
@@ -6,6 +15,7 @@ const ArticleCard = ({article}) => {
             <img src={article.article_img_url} alt="article image" className="article-img"/>
             <p>Votes: {article.votes}</p>
             <p>{article.comment_count} comments have been left on this post</p>
+            <p>posted: {formattedDate}</p>
         </>
     )
 }
